fcde4862d513a30f13dce70a3806e72681784b62
trontzal <trontzal@gmail.com>
2023-05-15 12:57:23 +0200

    se dan la vuelta todas las cartas pero avances

--- src/components/JuegoCartasComp.vue
+++ src/components/JuegoCartasComp.vue
@@ -2,8 +2,13 @@
     <div class="componente">
         <h2>componente cartas</h2>
         <div v-if="juegoIniciado == true" class="juegoIniciado">
-            <div v-for="(carta, index) in cartasDesordenadas" :key="index">
-                {{ carta }}
+            <div v-for="(carta, index) in cartasDesordenadas" :key="index" @click="seleccionarCarta(carta)">
+                <div v-if="verCarta == true" class="ensenando">
+                    {{ carta }}
+                </div>
+                <div v-if="verCarta == false" class="sinEnsenar">
+
+                </div>
             </div>
         </div>
         <button @click="iniciarJuego">Empezar</button>
@@ -15,9 +20,9 @@
         name:"JuegoCartas",
         data(){
             return{
-                
+                verCarta : false,
                 juegoIniciado : false,
-                cartas : [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8],
+                cartas : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
                 cartasDesordenadas:[]
             }
         },
@@ -39,7 +44,32 @@
 
             iniciarJuego(){
                 this.juegoIniciado = true;
+            },
+
+            seleccionarCarta(carta){
+                console.log(carta);
+                this.verCarta = true;
             }
         }
     }
 </script>
+
+<style scoped>
+
+    *{
+        border: 1px solid black;
+    }
+    .juegoIniciado{
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        justify-content: center;
+        width: fit-content;
+        margin: 0 auto;
+    }
+
+    .juegoIniciado > div{
+        height: 3.5rem;
+        width: 3rem;
+        margin: 0.5rem;
+    }
+</style>
