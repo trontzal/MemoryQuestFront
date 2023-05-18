ed74abd17cf58ba8003a8fd2749270c1f903502c
trontzal <trontzal@gmail.com>
2023-05-18 14:19:57 +0200

    np veo los datos de python aun

---
+++ src/components/EjemploRecivirComp.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="datos">
+        <p>{{ datos.mensaje }}</p>
+        <p>Numero: {{ datos.numero }}</p>
+        <ul>
+            <li v-for="item in datos.lista" :key="item">{{ item }}</li>
+        </ul>
+    </div>
+    <div v-else>
+        no sale
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios'
+
+    export default{
+        name: "RecivirEjemplo",
+        data(){
+            return{
+                datos: null
+            };
+        },
+        mounted(){
+            axios.get('http://localhost:5000/ejemplo')
+            .then(response => {
+                this.datos = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    }
+</script>
--- src/views/MemoriaVisual.vue
+++ src/views/MemoriaVisual.vue
@@ -1,14 +1,17 @@
 <template>
     <MemoriaVisualComp/>
+    <EjemploRecivirComp/>
 </template>
 
 <script>
     import MemoriaVisualComp from "../components/MemoriaVisualComp.vue"
+    import EjemploRecivirComp from "@/components/EjemploRecivirComp.vue";
 
     export default{
         name: 'MemoriaVisual',
         components:{
-            MemoriaVisualComp
+            MemoriaVisualComp,
+            EjemploRecivirComp
         },
     }
 </script>
