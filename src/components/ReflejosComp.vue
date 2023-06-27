8d17c3c72016ebc0b04a5dd98648baa22c08708c
Trontzal <trontzal@gmail.com>
2023-06-27 14:22:46 +0200

    chatgpt pero no fincoona, provisional

--- src/components/ReflejosComp.vue
+++ src/components/ReflejosComp.vue
@@ -1,45 +1,42 @@
 <template>
     <div class="comp color2">
-        <h2 v-if="juegoIniciado == false">Juego de Reflejos</h2>
-        <div v-if="juegoIniciado == false" class="vistaInicial">
+        <h2 v-if="!juegoIniciado">Juego de Reflejos</h2>
+
+        <div v-if="!juegoIniciado" class="vistaInicial">
             <img src="" alt="icono-de-reflejos">
-            <div v-if="resultado == false" class="descripcion">
-                <p>En el juego de Reflejos Rápidos, tu objetivo es hacer clic lo más rápido posible cuando la pantalla se ilumine en verde, evitando hacer clic cuando se ilumine en naranja. Pon a prueba tus reflejos y consigue la mejor puntuación evitando los colores equivocados.</p>
+            <div v-if="!resultado" class="descripcion">
+                <p>
+                    En el juego de Reflejos Rápidos, tu objetivo es hacer clic lo más
+                    rápido posible cuando la pantalla se ilumine en verde, evitando hacer
+                    clic cuando se ilumine en naranja. Pon a prueba tus reflejos y
+                    consigue la mejor puntuación evitando los colores equivocados.
+                </p>
             </div>
             <div v-if="resultado" class="resultado">
                 <p>Aquí sale el resultado al terminar el juego</p>
                 <p>Tiempo total: {{ tiempoTotal }} ms</p>
             </div>
         </div>
+
         <div v-if="juegoIniciado" class="juego">
             <div>
-                <div v-if="juegoIniciado2 == true" :class="juegoIniciado2">
-                    <div v-if="verde ==true" class="verde" @click="acierto = true">
-                        
-                    </div>
-                    <div v-if="naranja" class="naranja">
-                    
-                    </div>
-                    <div v-else>
-                        
-                    </div>
+                <div v-if="juegoIniciado2">
+                    <div v-if="verde" class="verde" @click="verdeClicado"></div>
+                    <div v-if="naranja" class="naranja" @click="naranjaClicada"></div>
                 </div>
-                <div v-else class="preparado" @click="generarColores()">
-                    <p>Haz click cuando estes preparado</p>
+                <div v-else class="preparado" @click="iniciarJuego">
+                    <p>Haz clic cuando estés preparado</p>
                 </div>
             </div>
         </div>
 
-        <button @click="iniciarJuego" v-if="empezar" class="btn">Preparado</button>
+        <button v-if="empezar" class="btn" @click="iniciarJuego">Preparado</button>
     </div>
 </template>
 
 <script>
-
 export default {
     name: 'PlantillaComp',
-    components: {
-    },
     data() {
         return {
             empezar: true,
@@ -48,46 +45,67 @@ export default {
             juegoIniciado2: false,
             verde: false,
             naranja: false,
-            acierto: false,
-        }
+            tiempoInicio: 0,
+            tiempoTotal: 0,
+            timeoutID: null,
+        };
     },
     methods: {
-        iniciarJuego(){
+        iniciarJuego() {
             this.juegoIniciado = true;
             this.empezar = false;
+            this.generarColores();
         },
         generarColores() {
             this.juegoIniciado2 = true;
-            const aleatorio = Math.random();
-            this.verde = aleatorio < 0.5;
-            this.naranja = aleatorio >= 0.5;
+            this.verde = false;
+            this.naranja = false;
+            this.timeoutID = setTimeout(() => {
+                const aleatorio = Math.random();
+                this.verde = aleatorio < 0.5;
+                this.naranja = aleatorio >= 0.5;
+                this.tiempoInicio = Date.now();
 
+                if (this.naranja) {
+                    this.timeoutID = setTimeout(() => {
+                        this.generarColores();
+                    }, 2000);
+                }
+            }, Math.random() * 5000);
+        },
+        verdeClicado() {
+            if (this.verde) {
+                const tiempoFin = Date.now();
+                this.tiempoTotal = tiempoFin - this.tiempoInicio;
+                window.alert(`¡Correcto! Tardaste ${this.tiempoTotal.toString()} ms en hacer clic.`);
+                this.reiniciarJuego();
+            }
+        },
+
+
+        naranjaClicada() {
             if (this.naranja) {
-                this.esNaranja()
-            }else {
-                this.esVerde()
+                window.alert('¡Incorrecto! Hiciste clic en naranja. Inténtalo de nuevo.');
+                this.reiniciarJuego();
             }
         },
-
-        esNaranja(){
-            setTimeout(() => {
-                this.naranja = false;
-                setTimeout(() => {
-                    this.generarColores();
-                }, 2000 + Math.random() * 5000); // Tiempo aleatorio en milisegundos
-            }, 2000);
+        reiniciarJuego() {
+            clearTimeout(this.timeoutID);
+            this.juegoIniciado = false;
+            this.resultado = true;
+            this.juegoIniciado2 = false;
+            this.verde = false;
+            this.naranja = false;
+            this.tiempoInicio = 0;
+            this.tiempoTotal = 0;
         },
-
-        esVerde() {
-            if (this.acierto) {
-                window.alert('¡Correcto! Hiciste clic en el color verde.');
-            }
-        },
-
-
-    }
-}
+    },
+};
 </script>
+
+
+
+
 
 <style scoped>
 
